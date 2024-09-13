05353c6b7bf8f703836b531115ee031714259fc9
mannycepeda1989 <100322499+mannycepeda1989@users.noreply.github.com>
2024-09-13 10:37:56 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgram {
+
+  /* This is my first java program.
+  * This will print 'Hello World' as the output
+  */
+  
+  public static void main(String []args) {
+    System.out.println("Hello World"); // prints Hello World
+    public String secret = "wss2389eryuifrh3w2rfyew8y023yeruwi=";
+  }
+}
